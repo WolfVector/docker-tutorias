3337d8bb61b54c5fc434631795e13dd70a052ff8
capodesinaloa <64706999+capodesinaloa@users.noreply.github.com>
2022-11-13 15:24:37 -0600

    [Test] Store Form
    Ya se puede guardar el contenido del formulario

--- web_project/pages/index.js
+++ web_project/pages/index.js
@@ -3,9 +3,12 @@ import { useRef } from 'react'
 import { handleAsyncReq } from '../lib/util'
 import styles from '../styles/Home.module.css'
 
-
 export default function Home() {
-  const input_test = useRef()
+  const input_name = useRef()
+  const input_matricula = useRef()
+  const input_carrera = useRef()
+  const input_semestre = useRef()
+  const input_cicloescolar = useRef()
 
   /*
   Para enviar datos del formulario a la db, se puede realizar utilizando la función fetch() de javascript o la función handleAsyncReq que 
@@ -15,9 +18,13 @@ export default function Home() {
   */
 
   
-  async function sendTest() {
+  async function SendData() {
     let data_obj = {
-      value1: input_test.current.value //Obtenemos el valor del input
+      name: input_name.current.value, //Obtenemos el valor del input
+      matricula: input_matricula.current.value,
+      carrera: input_carrera.current.value,
+      semestre: input_semestre.current.value,
+      ciclo_escolar: input_cicloescolar.current.value
     }
 
     //Dentro de /api/test viene una función 'handler' con parametros req y res.
@@ -42,11 +49,22 @@ export default function Home() {
 
   return (
     <div>
-      Hola mundo 
-      
-      <form>
-        <input type="text" className="input_class" ref={input_test} />
-        <button type='button' onClick={sendTest}>Enviar</button>
+      <form /*onSubmit={SendData}*/>
+        Nombre del estudiante&nbsp;
+        <input type="text" className="input_class" size="30" ref={input_name} />&nbsp;&nbsp;
+        Matricula&nbsp;
+        <input type="text" className="input_class" size="6" maxLength="6" ref={input_matricula} />
+        <br></br>
+
+        Carrera&nbsp;
+        <input type="text" className="input_class" size="5" ref={input_carrera} />&nbsp;&nbsp;
+        Semestre&nbsp;
+        <input type="text" className="input_class" size="5" ref={input_semestre} />
+        Ciclo escolar&nbsp;
+        <input type="text" className="input_class" size="5" ref={input_cicloescolar} />
+        <br></br>
+        <button type='button'>Enviar</button> {/* tipo se cambia a submit cuando quede bien calado */}
+        
       </form>
     </div>
   )
